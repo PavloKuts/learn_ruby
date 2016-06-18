9594df33614670a2cca74344d1d5d2f2ece8d4ed
Serge Bedzhyk <smileart21@gmail.com>
2016-06-18 19:46:22 +0300

    Implement class/object hierarchy generator

---
+++ hierarchy.rb
@@ -0,0 +1,41 @@
+class Hierarchy
+  def initialize(object)
+    @object = object
+  end
+
+  def generate
+    if @object.respond_to?(:name)
+      orig_class = constantize(@object.name)
+    else
+      orig_class = @object.class
+    end
+
+    padding = 0
+    begin
+      puts "#{'-' * padding}~> #{orig_class.to_s}"
+      padding += 2
+    end while orig_class = orig_class.superclass
+    puts
+  end
+
+  private
+
+  def constantize(camel_cased_word)
+    names = camel_cased_word.split('::')
+    names.shift if names.empty? || names.first.empty?
+
+    constant = Object
+    names.each do |name|
+      constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+    end
+    constant
+  end
+end
+
+if $0 == __FILE__
+  Hierarchy.new(3).generate
+  Hierarchy.new(Hierarchy.new(3)).generate
+  Hierarchy.new(String).generate
+  Hierarchy.new('test').generate
+  Hierarchy.new(nil).generate
+end
